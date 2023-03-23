36832f58acda6e5502392fc5234a603ad25c3a3f
Kai Winter <kaiwinter@gmx.de>
2023-03-23 18:26:19 +0100

    Spring Boot 3 update and latest results

--- src/main/java/pl/aurora/projections/domain/sample/service/SampleService.java
+++ src/main/java/pl/aurora/projections/domain/sample/service/SampleService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Tuple;
+import jakarta.persistence.Tuple;
 import java.util.List;
 
 @Service
